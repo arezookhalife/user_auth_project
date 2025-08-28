b3d414fbd956ef568675dabc1633bb047d86366b
arezoo <arezoo.kh1988@gmail.com>
2025-08-28 18:05:56 +0330

    DAY3: Add user profile view and edit functionality with validation and password hashing

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,11 @@ from . import views
 app_name = "accounts"
 
 urlpatterns = [
+    path("", views.accounts_home, name="home"),
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
+    path("profile/", views.profile_view, name="profile"),
+    path("profile/edit/", views.edit_profile_view, name="edit_profile"),
 ]
